fd0428a83bfc0d612b550d69c73691057af74779
botnyx <chlsbkrs@gmail.com>
2019-03-06 17:17:17 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,56 @@
-<?php
+<?php 
+/* 
+	Parse Config with sections 
+	
+	by default, configs are loaded by hostname
+	
+	Example: 
+		testsite-com.conf.ini
+		my-testsite-com.conf.ini
+
+*/
+$_conf = str_replace('www-','',str_replace('.','-',$_SERVER['HTTP_HOST']).".conf.ini");
+
+if( file_exists( "../".$_conf ) ){
+	define( '_SETTINGS',  parse_ini_file( "../".$_conf , true)) ;
+}else{
+	define( '_SETTINGS',  parse_ini_file("../configuration.ini", true)) ;
+}
+	
+
+
+
+/* quit if no settings.*/ 
+if(_SETTINGS==false){die("check configuration.ini");}
+
+/* Composer autoloader */
+require _SETTINGS['paths']['root'] .'/vendor/autoload.php';
+
+
+
+
+
+use Slim\Http;
+use Slim\Views;
+
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\Cache\PredisCache;
+
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
+use Kevinrob\GuzzleCache\Strategy\PublicCacheStrategy;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+
+use Kevinrob\GuzzleCache\KeyValueHttpHeader;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+
+
+#use \Psr\Http\Message\ServerRequestInterface as Request;
+#use \Psr\Http\Message\ResponseInterface as Response;
+
