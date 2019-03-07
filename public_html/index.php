73e5d678e2869d8c0f47f94bf288a72ef6948d08
botnyx <chlsbkrs@gmail.com>
2019-03-07 11:41:35 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -32,19 +32,3 @@ require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/incl
 
 /* Finally, run the app. */
 $app->run();
-
-
-#use Slim\Http;
-#use Slim\Views;
-#use Psr\Http\Message\ServerRequestInterface;
-#use Psr\Http\Message\ResponseInterface;
-#use Doctrine\Common\Cache\FilesystemCache;
-#use Doctrine\Common\Cache\PredisCache;
-#use Kevinrob\GuzzleCache\CacheMiddleware;
-#use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
-#use Kevinrob\GuzzleCache\Strategy\PublicCacheStrategy;
-#use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
-#use Kevinrob\GuzzleCache\KeyValueHttpHeader;
-#use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
-#use \Psr\Http\Message\ServerRequestInterface as Request;
-#use \Psr\Http\Message\ResponseInterface as Response;
