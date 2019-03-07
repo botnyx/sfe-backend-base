0d558a55d81c8e28f09919e9152c129781046f2d
botnyx <chlsbkrs@gmail.com>
2019-03-07 02:08:59 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -38,20 +38,15 @@ $app->run();
 
 #use Slim\Http;
 #use Slim\Views;
-
 #use Psr\Http\Message\ServerRequestInterface;
 #use Psr\Http\Message\ResponseInterface;
-
 #use Doctrine\Common\Cache\FilesystemCache;
 #use Doctrine\Common\Cache\PredisCache;
-
 #use Kevinrob\GuzzleCache\CacheMiddleware;
 #use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
 #use Kevinrob\GuzzleCache\Strategy\PublicCacheStrategy;
 #use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
-
 #use Kevinrob\GuzzleCache\KeyValueHttpHeader;
 #use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
-
 #use \Psr\Http\Message\ServerRequestInterface as Request;
 #use \Psr\Http\Message\ResponseInterface as Response;
