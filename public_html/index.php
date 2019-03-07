6f92486b0e894b363ca5cdc1fa3f3c47ce3b51ad
botnyx <chlsbkrs@gmail.com>
2019-03-07 02:07:09 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,14 @@
-<?php 
+<?php
 
 /* Load the Configuration */
-if( !file_exists( "../configuration.ini" ) ){
-	die("check configuration.ini");
-}else{
+if( !file_exists( "../configuration.ini" ) ){ die("check configuration.ini"); }else{
 	define( '_SETTINGS',  parse_ini_file("../configuration.ini", true)) ;
 }
-	
 
 /* Composer autoloader */
 require _SETTINGS['paths']['root'] .'/vendor/autoload.php';
 /* Include the shared core, which adds the deps, middleware and routes to the app. */
 require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/dependencies.php");
-
-
 
 
 /* Create the Sfe instance with settings. */
@@ -27,19 +22,24 @@ $app = $sfe->start();
 /* Setup the container  */
 $container = $app->getContainer();
 
+/* Include the middleware. */
+require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/container.php");
 
+/* Include the middleware. */
+require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/middleware.php");
 
+/* Include the routes. */
+require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/routes.php");
 
+/* Finally, run the app. */
+$app->run();
 
 
 #use Slim\Http;
 #use Slim\Views;
 
-
-
 #use Psr\Http\Message\ServerRequestInterface;
 #use Psr\Http\Message\ResponseInterface;
-
 
 #use Doctrine\Common\Cache\FilesystemCache;
 #use Doctrine\Common\Cache\PredisCache;
@@ -54,20 +54,3 @@ $container = $app->getContainer();
 
 #use \Psr\Http\Message\ServerRequestInterface as Request;
 #use \Psr\Http\Message\ResponseInterface as Response;
-
-
-
-
-/* Include the middleware. */
-require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/container.php");
-
-/* Include the middleware. */
-require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/middleware.php");
-
-/* Include the routes. */
-require_once(_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/routes.php");
-
-
-
-/* Finally, run the app. */
-$app->run();
